f6a1dc1e66154f5dd8c4447ff1275ba2286424a2
Andy Park <atpark333@gmail.com>
2014-05-07 22:55:30 +0900

    REFACTOR: Change kana maps to use roumaji as keys, remove extraneous code, and write more service unit tests

--- src/services/kana.js
+++ src/services/kana.js
@@ -3,141 +3,84 @@
 angular.module('ap.kana').factory('kanaService', function (bulkReplace) {
 
     var hiraganaMonographs = {
-        "あ": "A", "い": "I", "う": "U", "え": "E", "お": "O",
-        "か": "KA", "き": "KI", "く": "KU", "け": "KE", "こ": "KO",
-        "さ": "SA", "し": "SHI", "す": "SU", "せ": "SE", "そ": "SO",
-        "た": "TA", "ち": "CHI", "つ": "TSU", "て": "TE", "と": "TO",
-        "な": "NA", "に": "NI", "ぬ": "NU", "ね": "NE", "の": "NO",
-        "は": "HA", "ひ": "HI", "ふ": "FU", "へ": "HE", "ほ": "HO",
-        "ま": "MA", "み": "MI", "む": "MU", "め": "ME", "も": "MO",
-        "や": "YA", "ゆ": "YU", "よ": "YO",
-        "ら": "RA", "り": "RI", "る": "RU", "れ": "RE", "ろ": "RO",
-        "わ": "WA", "ゐ": "WI", "ゑ": "WE", "を": "WO", "ん": "NN",
-        "が": "GA", "ぎ": "GI", "ぐ": "GU", "げ": "GE", "ご": "GO",
-        "ざ": "ZA", "じ": "JI", "ず": "ZU", "ぜ": "ZE", "ぞ": "ZO",
-        "だ": "DA", "ぢ": "DJI", "づ": "DZU", "で": "DE", "ど": "DO",
-        "ば": "BA", "び": "BI", "ぶ": "BU", "べ": "BE", "ぼ": "BO",
-        "ぱ": "PA", "ぴ": "PI", "ぷ": "PU", "ぺ": "PE", "ぽ": "PO"
+        "a": "あ", "i": "い", "u": "う", "e": "え", "o": "お",
+        "ka": "か", "ki": "き", "ku": "く", "ke": "け", "ko": "こ",
+        "sa": "さ", "shi": "し", "su": "す", "se": "せ", "so": "そ",
+        "ta": "た", "chi": "ち", "tsu": "つ", "te": "て", "to": "と",
+        "na": "な", "ni": "に", "nu": "ぬ", "ne": "ね", "no": "の",
+        "ha": "は", "hi": "ひ", "fu": "ふ", "he": "へ", "ho": "ほ",
+        "ma": "ま", "mi": "み", "mu": "む", "me": "め", "mo": "も",
+        "ya": "や", "yu": "ゆ", "yo": "よ",
+        "ra": "ら", "ri": "り", "ru": "る", "re": "れ", "ro": "ろ",
+        "wa": "わ", "wi": "ゐ", "we": "ゑ", "wo": "を", "n": "ん",
+        "ga": "が", "gi": "ぎ", "gu": "ぐ", "ge": "げ", "go": "ご",
+        "za": "ざ", "ji": "じ", "zu": "ず", "ze": "ぜ", "zo": "ぞ",
+        "da": "だ", "djI": "ぢ", "dzU": "づ", "de": "で", "do": "ど",
+        "ba": "ば", "bi": "び", "bu": "ぶ", "be": "べ", "bo": "ぼ",
+        "pa": "ぱ", "pi": "ぴ", "pu": "ぷ", "pe": "ぺ", "po": "ぽ"
     };
 
     var hiraganaDigraphs = {
-        "きゃ": "KYA", "きゅ": "KYU", "きょ": "KYO",
-        "しゃ": "SHA", "しゅ": "SHU", "しょ": "SHO",
-        "ちゃ": "CHA", "ちゅ": "CHU", "ちょ": "CHO",
-        "にゃ": "NYA", "にゅ": "NYU", "にょ": "NYO",
-        "ひゃ": "HYA", "ひゅ": "HYU", "ひょ": "HYO",
-        "みゃ": "MYA", "みゅ": "MYU", "みょ": "MYO",
-        "りゃ": "RYA", "りゅ": "RYU", "りょ": "RYO",
-        "ぎゃ": "GYA", "ぎゅ": "GYU", "ぎょ": "GYO",
-        "じゃ": "JA", "じゅ": "JU", "じょ": "JO",
-        "びゃ": "BYA", "びゅ": "BYU", "びょ": "BYO",
-        "ぴゃ": "PYA", "ぴゅ": "PYU", "ぴょ": "PYO"
+        "kya": "きゃ", "kyu": "きゅ", "kyo": "きょ",
+        "sha": "しゃ", "shu": "しゅ", "sho": "しょ",
+        "cha": "ちゃ", "chu": "ちゅ", "cho": "ちょ",
+        "nya": "にゃ", "nyu": "にゅ", "nyo": "にょ",
+        "hya": "ひゃ", "hyu": "ひゅ", "hyo": "ひょ",
+        "mya": "みゃ", "myu": "みゅ", "myo": "みょ",
+        "rya": "りゃ", "ryu": "りゅ", "ryo": "りょ",
+        "gya": "ぎゃ", "gyu": "ぎゅ", "gyo": "ぎょ",
+        "ja": "じゃ", "ju": "じゅ", "jo": "じょ",
+        "bya": "びゃ", "byu": "びゅ", "byo": "びょ",
+        "pya": "ぴゃ", "pyu": "ぴゅ", "pyo": "ぴょ"
     };
 
     var katakanaMonographs = {
-        "ア": "A", "イ": "I", "ウ": "U", "エ": "E", "オ": "O",
-        "カ": "KA", "キ": "KI", "ク": "KU", "ケ": "KE", "コ": "KO",
-        "サ": "SA", "シ": "SHI", "ス": "SU", "セ": "SE", "ソ": "SO",
-        "タ": "TA", "チ": "CHI", "ツ": "TSU", "テ": "TE", "ト": "TO",
-        "ナ": "NA", "ニ": "NI", "ヌ": "NU", "ネ": "NE", "ノ": "NO",
-        "ハ": "HA", "ヒ": "HI", "フ": "FU", "ヘ": "HE", "ホ": "HO",
-        "マ": "MA", "ミ": "MI", "ム": "MU", "メ": "ME", "モ": "MO",
-        "ヤ": "YA", "ユ": "YU", "ヨ": "YO",
-        "ラ": "RA", "リ": "RI", "ル": "RU", "レ": "RE", "ロ": "RO",
-        "ワ": "WA", "ヰ": "WI", "ヱ": "WE",  "ヲ": "WO", "ン": "NN",
-        "ガ": "GA", "ギ": "GI", "グ": "GU", "ゲ": "GE", "ゴ": "GO",
-        "ザ": "ZA", "ジ": "JI", "ズ": "ZU", "ゼ": "ZE", "ゾ": "ZO",
-        "ダ": "DA", "ヂ": "DJI", "ヅ": "DZU", "デ": "DE", "ド": "DO",
-        "バ": "BA", "ビ": "BI", "ブ": "BU", "ベ": "BE", "ボ": "BO",
-        "パ": "PA", "ピ": "PI", "プ": "PU", "ペ": "PE", "ポ": "PO"
+        "a": "ア", "i": "イ", "u": "ウ", "e": "エ", "o": "オ",
+        "ka": "カ", "ki": "キ", "ku": "ク", "ke": "ケ", "ko": "コ",
+        "sa": "サ", "shi": "シ", "su": "ス",  "se": "セ", "so": "ソ",
+        "ta": "タ", "chi": "チ", "tsu": "ツ", "te": "テ", "to": "ト",
+        "na": "ナ", "ni": "ニ", "nu": "ヌ", "ne": "ネ", "no": "ノ",
+        "ha": "ハ", "hi": "ヒ", "fu": "フ", "he": "ヘ", "ho": "ホ",
+        "ma": "マ", "mi": "ミ", "mu": "ム", "me": "メ", "mo": "モ",
+        "ya": "ヤ", "yo": "ヨ", "yu": "ユ",
+        "ra": "ラ", "ri": "リ", "ru": "ル",  "re": "レ", "ro": "ロ",
+        "wa": "ワ", "wi": "ヰ", "we": "ヱ", "wo": "ヲ", "n": "ン",
+        "ga": "ガ", "gi": "ギ", "gu": "ぐ", "ge": "ゲ", "go": "ゴ",
+        "za": "ザ", "ji": "ジ", "zu": "ズ", "ze": "ゼ", "zo": "ゾ",
+        "da": "ダ", "dji": "ヂ", "dzu": "ヅ", "de": "デ", "do": "ド",
+        "ba": "バ", "bi": "ビ", "bu": "ブ", "be": "ベ", "bo": "ボ",
+        "pa": "パ", "pi": "ピ", "pu": "プ", "pe": "ペ", "po": "ポ"
     };
 
     var katakanaDigraphs = {
-        "アー": "Ā", "イー": "Ī", "ウー": "Ū", "エー": "Ē", "オー": "Ō",
-        "カー": "KĀ", "キー": "KĪ", "クー": "KŪ", "ケー": "KĒ", "コー": "KŌ",
-        "サー": "SĀ", "シー": "SHĪ", "スー": "SŪ", "セー": "SĒ", "ソー": "SŌ",
-        "ター": "TĀ", "チー": "CHĪ", "ツー": "TSŪ", "テー": "TĒ", "トー": "TŌ",
-        "ナー": "NĀ", "ニー": "NĪ", "ヌー": "NŪ", "ネー": "NĒ", "ノー": "NŌ",
-        "ハー": "HĀ", "ヒー": "HĪ", "フー": "FŪ", "ヘー": "HĒ", "ホー": "HŌ",
-        "マー": "MĀ", "ミー": "MĪ", "ムー": "MŪ", "メー": "MĒ", "モー": "MŌ",
-        "ヤー": "YĀ", "ユー": "YŪ", "ヨー": "YŌ",
-        "ラー": "RĀ", "リー": "RĪ", "ルー": "RŪ", "レー": "RĒ", "ロー": "RŌ",
-        "ワー": "WĀ", "ヰー": "WĪ", "ヱー": "WĒ",  "ヲー": "WŌ", "ンー": "N",
-        "ガー": "GĀ", "ギー": "GĪ", "グー": "GŪ", "ゲー": "GĒ", "ゴー": "GŌ",
-        "ザー": "ZĀ", "ジー": "JĪ", "ズー": "ZŪ", "ゼー": "ZĒ", "ゾー": "ZŌ",
-        "ダー": "DĀ", "ヂー": "DJĪ", "ヅー": "DZŪ", "デー": "DĒ", "ドー": "DŌ",
-        "バー": "BĀ", "ビー": "BĪ", "ブー": "BŪ", "ベー": "BĒ", "ボー": "BŌ",
-        "パー": "PĀ", "ピー": "PĪ", "プー": "PŪ", "ペー": "PĒ", "ポー": "PŌ",
-        "キャ": "KYA", "キュ": "KYU", "キョ": "KYO",
-        "シャ": "SHA", "シュ": "SHU", "ショ": "SHO",
-        "チャ": "CHA", "チュ": "CHU", "チョ": "CHO",
-        "ニャ": "NYA", "ニュ": "NYU", "ニョ": "NYO",
-        "ヒャ": "HYA", "ヒュ": "HYU", "ヒョ": "HYO",
-        "ミャ": "MYA", "ミュ": "MYU", "ミョ": "MYO",
-        "リャ": "RYA", "リュ": "RYU", "リョ": "RYO",
-        "ギャ": "GYA", "ギュ": "GYU", "ギョ": "GYO",
-        "ジャ": "JA", "ジュ": "JU", "ジョ": "JO",
-        "ビャ": "BYA", "ビュ": "BYU", "ビョ": "BYO",
-        "ピャ": "PYA", "ピュ": "PYU", "ピョ": "PYO"
+        "kyo": "キョ", "kyu": "キュ", "kya": "キャ",
+        "sho": "ショ", "shu": "シュ", "sha": "シャ",
+        "cho": "チョ", "chu": "チュ", "cha": "チャ",
+        "nyo": "ニョ", "nyu": "ニュ", "nya": "ニャ",
+        "hyo": "ヒョ", "hyu": "ヒュ", "hya": "ヒャ",
+        "myo": "ミョ", "myu": "ミュ", "mya": "ミャ",
+        "ryo": "リョ", "ryu": "リュ", "rya": "リャ",
+        "gyo": "ギョ", "gyu": "ギュ", "gya": "ギャ",
+        "jo": "ジョ", "ju": "ジュ", "ja": "ジャ",
+        "byo": "ビョ", "byu": "ビュ", "bya": "ビャ",
+        "pyo": "ピョ", "pyu": "ピュ", "pya": "ピャ"
     };
 
-    var katakanaTrigraphs = {
-        "キャー": "KYĀ", "キュー": "KYŪ", "キョー": "KYŌ",
-        "シャー": "SHĀ", "シュー": "SHŪ", "ショー": "SHŌ",
-        "チャー": "CHĀ", "チュー": "CHŪ", "チョー": "CHŌ",
-        "ニャー": "NYĀ", "ニュー": "NYŪ", "ニョー": "NYŌ",
-        "ヒャー": "HYĀ", "ヒュー": "HYŪ", "ヒョー": "HYŌ",
-        "ミャー": "MYĀ", "ミュー": "MYŪ", "ミョー": "MYŌ",
-        "リャー": "RYĀ", "リュー": "RYŪ", "リョー": "RYŌ",
-        "ギャー": "GYĀ", "ギュー": "GYŪ", "ギョー": "GYŌ",
-        "ジャー": "JĀ", "ジュー": "JŪ", "ジョー": "JŌ",
-        "ビャー": "BYĀ", "ビュー": "BYŪ", "ビョー": "BYŌ",
-        "ピャー": "PYĀ", "ピュー": "PYŪ", "ピョー": "PYŌ"
-    };
-
-    // Used to convert old Nihon-Shiki style romaji into the modern Hepburn form.
-    // Source: http://nayuki.eigenstate.org/page/variations-on-japanese-romanization
-    var nihonShiki = {
-        "SI": "SHI",
-        "ZI": "JI",
-        "TI": "CHI",
-        "DI": "JI",
-        "TU": "TSU",
-        "DU": "ZU",
-        "SHU": "SHU", // Prevent HU from accidentally converting
-        "CHU": "CHU",
-        "HU": "FU",
-        "CYA": "CHA",
-        "CYO": "CHO",
-        "CYU": "CHU",
-        "SYA": "SHA",
-        "SYU": "SHU",
-        "SYO": "SHO",
-        "ZYA": "JA",
-        "ZYU": "JU",
-        "ZYO": "JO",
-        "TYA": "CHA",
-        "TYU": "CHU",
-        "TYO": "CHO",
-        "DYA": "JA",
-        "DYU": "JU",
-        "DYO": "JO"
-    };
-
-    // For use with toHiragana
     var hiraganaMap = {};
 
-    Object.keys(hiraganaMonographs).forEach(function(key) {
+    Object.keys(hiraganaMonographs).forEach(function (key) {
         var value = hiraganaMonographs[key];
-        if (!(value in hiraganaMap)) {
-            hiraganaMap[value] = key;
+
+        if (!(key in hiraganaMap)) {
+            hiraganaMap[key] = value;
         }
     });
 
-    Object.keys(hiraganaDigraphs).forEach(function(key) {
+    Object.keys(hiraganaDigraphs).forEach(function (key) {
         var value = hiraganaDigraphs[key];
-        if (!(value in hiraganaMap)) {
-            hiraganaMap[value] = key;
+
+        if (!(key in hiraganaMap)) {
+            hiraganaMap[key] = value;
         }
     });
 
@@ -145,29 +88,24 @@ angular.module('ap.kana').factory('kanaService', function (bulkReplace) {
         return b.length - a.length;
     }).join("|"), "g");
 
-    // For use with toKatakana
     var katakanaMap = {};
 
-    Object.keys(katakanaMonographs).forEach(function(key) {
+    Object.keys(katakanaMonographs).forEach(function (key) {
         var value = katakanaMonographs[key];
-        if (!(value in katakanaMap)) {
-            katakanaMap[value] = key;
+
+        if (!(key in katakanaMap)) {
+            katakanaMap[key] = value;
         }
     });
 
-    Object.keys(katakanaDigraphs).forEach(function(key) {
+    Object.keys(katakanaDigraphs).forEach(function (key) {
         var value = katakanaDigraphs[key];
-        if (!(value in katakanaMap)) {
-            katakanaMap[value] = key;
+
+        if (!(key in katakanaMap)) {
+            katakanaMap[key] = value;
         }
     });
 
-    Object.keys(katakanaTrigraphs).forEach(function(key) {
-        var value = katakanaTrigraphs[key];
-        if (!(value in katakanaMap)) {
-            katakanaMap[value] = key;
-        }
-    });
 
     var katakanaRegex = new RegExp(Object.keys(katakanaMap).sort(function(a, b) {
         return b.length - a.length;
@@ -176,36 +114,27 @@ angular.module('ap.kana').factory('kanaService', function (bulkReplace) {
 
     return {
         toHiragana: function (str) {
-            // All conversion is done in upper-case
-            str = str.toUpperCase();
+            // All conversion is done in lower-case
+            str = str.toLowerCase();
 
-            // Correct use of sokuon
-            str = str.replace(/TC/g, "っC");
+            str = str.replace(/([^aeioun])\1/g, "っ$1");
+            str = str.replace(/nn/g, "ん");
 
             // Transliteration
             str = bulkReplace.replace(str, hiraganaRegex, hiraganaMap);
-
-            // Fix any remaining N/M usage (that isn't a N' usage)
-            // TODO: This does not work with realtime translation. Find alternate
-            //       solution.
-            //str = str.replace(/N|M/g, "ん");
 
             return str;
         },
 
         toKatakana: function (str) {
-            // All conversion is done in upper-case
-            str = str.toUpperCase();
+            // All conversion is done in lower-case
+            str = str.toLowerCase();
 
-            // Correct use of sokuon
-            str = str.replace(/TC/g, "ッC");
-            str = str.replace(/([^AEIOUN])\1/g, "ッ$1");
+            str = str.replace(/([^aeioun])\1/g, "ッ$1");
+            str = str.replace(/nn/g, "ン");
 
             // Transliteration
             str = bulkReplace.replace(str, katakanaRegex, katakanaMap);
-
-            // Fix any remaining N/M usage (that isn't a N' usage)
-            str = str.replace(/N|M/g, "ン");
 
             return str;
         }
--- test/unit/services/kana.spec.js
+++ test/unit/services/kana.spec.js
@@ -9,12 +9,39 @@ describe('angular-kana: kanaService', function () {
         this.kanaService = kanaService;
     }));
 
-    it('should translate to hiragana', function () {
-        expect(this.kanaService.toHiragana('hiragana')).toBe('ひらがな');
+    describe('toHiragana', function () {
+        it('should transliterate to hiragana', function () {
+            expect(this.kanaService.toHiragana('hiragana')).toBe('ひらがな');
+        });
+
+        it('should not transliterate unmatched patterns for hiragana', function () {
+            expect(this.kanaService.toHiragana('sushix')).toBe('すしx');
+        });
+
+        it('should correctly transliterate double \'n\' to hiragana', function () {
+            expect(this.kanaService.toHiragana('mann')).toBe('まん');
+        });
+
+        it('should correctly transliterate double consonants to hiragana', function () {
+            expect(this.kanaService.toHiragana('natto')).toBe('なっと');
+        });
     });
 
-    it('should translate to katakana', function () {
-        expect(this.kanaService.toKatakana('katakana')).toBe('カタカナ');
-    });
+    describe('toKatakana', function () {
+        it('should translate to katakana', function () {
+            expect(this.kanaService.toKatakana('katakana')).toBe('カタカナ');
+        });
 
+        it('should not transliterate unmatched patterns for katakana', function () {
+            expect(this.kanaService.toKatakana('sushix')).toBe('スシx');
+        });
+
+        it('should correctly transliterate double \'n\' to katakana', function () {
+            expect(this.kanaService.toKatakana('mann')).toBe('マン');
+        });
+
+        it('should correctly transliterate double consonants to katakana', function () {
+            expect(this.kanaService.toKatakana('natto')).toBe('ナット');
+        });
+    });
 });
