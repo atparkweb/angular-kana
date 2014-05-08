b7bdc7226238a0caf8093dbfb2f02374f0305688
Andy Park <atpark333@gmail.com>
2014-05-09 00:57:55 +0900

    Start writing unit tests for directive

--- test/unit/directives/to_kana.spec.js
+++ test/unit/directives/to_kana.spec.js
@@ -1,3 +1,17 @@
 describe('angular-kana: toKana directive', function () {
-    //TODO: Implement toKana directive unit test
+    var controller;
+
+    beforeEach(function () {
+        angular.mock.module('atparkweb.kana');
+    });
+
+    beforeEach(inject(function ($rootScope) {
+        scope = $rootScope.$new();
+    }));
+
+    it('should have a scope', function () {
+        expect(scope).toBeDefined();
+    });
+
+    // TODO: Implement unit tests for toKana directive
 });
