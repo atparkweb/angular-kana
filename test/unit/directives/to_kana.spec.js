3284f1c36435d96e3fdb357aaad4e24b7aa3c8b9
Andy Park <atpark333@gmail.com>
2014-05-15 16:04:14 +0900

    Write directive unit tests

--- test/unit/directives/to_kana.spec.js
+++ test/unit/directives/to_kana.spec.js
@@ -1,5 +1,5 @@
 describe('angular-kana: toKana directive', function () {
-    var controller;
+    var scope, html, element, isolateScope;
 
     beforeEach(function () {
         angular.mock.module('atparkweb.kana');
@@ -13,5 +13,50 @@ describe('angular-kana: toKana directive', function () {
         expect(scope).toBeDefined();
     });
 
-    // TODO: Implement unit tests for toKana directive
+    describe('with string parameters', function () {
+        beforeEach(inject(function ($rootScope, $compile) {
+            html = '<input to-kana="\'hiragana\'" type="text" />';
+
+            element = $compile(html)(scope);
+
+            isolateScope = element.isolateScope();
+        }));
+
+        it('should have toKana defined on the scope', function () {
+            expect(isolateScope.toKana).toBeDefined();
+        });
+
+        it('should pass value of toKana attribute to scope', function () {
+            expect(isolateScope.toKana).toBe('hiragana');
+        });
+    });
+
+    describe('with scope parameters', function () {
+        beforeEach(inject(function ($rootScope, $compile) {
+            html = '<input to-kana="whichKana" type="text" />';
+            scope.whichKana = 'hiragana';
+
+            element = $compile(html)(scope);
+
+            isolateScope = element.isolateScope();
+        }));
+
+        it('should have a defined scope value', function () {
+            expect(isolateScope.toKana).toBeDefined();
+        });
+
+        it('should read initial scope value', function () {
+            expect(isolateScope.toKana).toBe('hiragana');
+        });
+
+        it('should be able to update toKana value through root scope', function () {
+            // Set a new value on the scope
+            scope.whichKana = 'katakana';
+
+            // Run $digest() to recompile DOM
+            scope.$digest();
+
+            expect(isolateScope.toKana).toBe('katakana');
+        });
+    });
 });
